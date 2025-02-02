8d1e67be213ebcb76063b1ae9c48f1f6130ae330
Бордюжа Марк <borduzamark@gmail.com>
2025-02-02 22:24:08 +0200

    Add files via upload

---
+++ Dz2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    std::cout << "Привіт світе!\n";
+    std::cout << "Мы здесь\n";
+
+    return 0;
+}
